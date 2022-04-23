ec70ed557eb57460250be46b2a80a2981ba78c73
Warren <warrenroque17@gmail.com>
2022-04-22 22:22:04 -0400

    refactor code for clearer naming

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,16 +4,17 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   return (
     <ul>
-      {props.days.map(day => {
-        return <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
-        />
-      })
-    }
+      {props.days.map((day) => {
+        return (
+          <DayListItem
+            key={day.id}
+            name={day.name}
+            spots={day.spots}
+            selected={day.name === props.value}
+            setDay={props.onChange}
+          />
+        );
+      })}
     </ul>
   );
 }
