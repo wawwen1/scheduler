95cb81f16beef59e655087e6f76918c916e7e320
Warren <warrenroque17@gmail.com>
2022-04-24 20:40:37 -0400

    create form component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    props.onCancel();
+    reset();
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+        onChange={event => setStudent(event.target.value)}
+        value={student}
+      />
+    </form>
+    <InterviewerList
+      interviewers={props.interviewers}
+      value={interviewer}
+      onChange={setInterviewer}
+    />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick={cancel}>Cancel</Button>
+      <Button confirm>Save</Button>
+    </section>
+  </section>
+</main>
+  );
+}
