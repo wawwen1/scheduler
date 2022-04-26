54628b6ce564276970c5d64448bad76be0325876
Warren <warrenroque17@gmail.com>
2022-04-26 12:26:21 -0400

    add getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,11 +4,11 @@ export function getAppointmentsForDay(state, day) {
 
   //excludes empty data + compare id with "state.appointment"
   if (filteredDays.length > 0) {
-    const appts = filteredDays[0].appointments.map((apptsID) => {
-      return state.appointments[apptsID];
+    const appointmentId = filteredDays[0].appointments.map((id) => {
+      return state.appointments[id];
     });
     //return matched value
-    return appts;
+    return appointmentId;
   }
   //return empty array
   return filteredDays;
@@ -25,4 +25,17 @@ export function getInterview(state, interview) {
   };
 
   return results;
+}
+
+export function getInterviewersForDay(state, day) {
+  const filteredDays = state.days.filter((res) => res.name === day);
+  if (filteredDays.length > 0) {
+    const interviewersId = filteredDays[0].interviewers.map((id) => {
+      return state.inteviewers[id];
+    });
+
+    return interviewersId;
+  }
+
+  return filteredDays;
 }
