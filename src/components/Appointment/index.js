346b2099cfeab1addb916506828fc82544ed8432
Warren <warrenroque17@gmail.com>
2022-04-27 01:25:05 -0400

    add deleting function + deleting mode + onDelete in Show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,33 +3,52 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 
 import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
-
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, props.interview).then(() => transition(SHOW));
+  }
+
+  function deleting() {
+    transition(DELETING);
+    props.cancelInterview(props.id, props.interview).then(() => transition(EMPTY));
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SAVING && <Status message={"Saving"} />}
+      {mode === DELETING && <Status message={"Deleting"} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={deleting}
         />
       )}
       {mode === CREATE && (
-        <Form interviewers={props.interviewers} onCancel={back} />
+        <Form interviewers={props.interviewers} onSave={save} onCancel={back} />
       )}
     </article>
   );
