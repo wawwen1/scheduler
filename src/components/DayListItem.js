417b98c48535a50118c3e4e287ba83f736427982
Warren <warrenroque17@gmail.com>
2022-04-29 03:40:20 -0400

    edit formatSpots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,23 +8,18 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
 
-  const formatSpots = () => {
-    let spots = "";
-
-    if (props.spots === 0) {
-      spots = "no spots remaining";
-      return spots;
-    } else if (props.spots === 1) {
-      spots = `${props.spots} spot remaining`;
-      return spots;
+  const formatSpots = spots => {
+    if (spots === 0) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return `1 spot remaining`;
     } else {
-      spots = `${props.spots} spots remaining`;
-      return spots;
+      return `${spots} spots remaining`;
     }
   };
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} data-testid="day">
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
